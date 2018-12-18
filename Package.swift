de7356fe7aa4bb46a01a1131b707607d304a8739
Kirill Titov <kirill@kirilltitov.com>
2018-12-18 16:02:42 +0300

    Updated Package.swift

--- Package.swift
+++ Package.swift
@@ -1,5 +1,16 @@
+// swift-tools-version:4.2
+
 import PackageDescription
 
 let package = Package(
-    name: "SwiftMsgPack"
+    name: "SwiftMsgPack",
+    products: [
+        .library(name: "SwiftMsgPack", targets: ["SwiftMsgPack"])
+    ],
+    dependencies: [],
+    targets: [
+        .target(name: "SwiftMsgPack", dependencies: []),
+        .testTarget(name: "SwiftMsgPackTests", dependencies: ["SwiftMsgPack"]),
+    ]
 )
+
